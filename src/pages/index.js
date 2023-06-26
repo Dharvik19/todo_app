149fa990b19b6f166a151558b0cac76a6473c04d
Dharvik19 <dharvik.badga22@gmail.com>
2023-06-26 21:42:10 +0530

    update the status

--- src/pages/index.js
+++ src/pages/index.js
@@ -21,7 +21,37 @@ export async function getStaticProps(){
 }
 function HomePage({notes}){
 
-  
+  const[visibility, setVisibility] = useState(false);
+  const [title,setTitle] = useState('');
+  const [content,setContent] = useState('');
+  const [noteId,setNoteId] = useState('');
+  const [completed, setCompleted] = useState(false);
+  const editForm = (title, content, noteId)=>{
+    setVisibility(visibility => !visibility);
+    setTitle(title);
+    setContent(content);
+    setNoteId(noteId);
+  }
+
+  const updateNote = async(noteId)=>{
+    const noteObj = {
+      title: title,
+      content: content
+    }
+    console.log(noteObj);
+
+    await axios.put(`api/updateNote?id=${noteId}`,noteObj)
+    .then(()=>{
+      window.location.reload(false);
+    })
+  }
+
+  const toggleTodo = async(noteId)=>{
+    const noteObj = {
+      completed : !completed
+    }
+    console.log(noteObj);
+  }
   return (
     <Fragment>
     <Head>
@@ -40,11 +70,28 @@ function HomePage({notes}){
           return(
             <li key={i}style={{backgroundColor:"red",borderRadius:"10px",padding:"10px",marginBottom:"10px"}}>
               <div className='m-1'>
-              <h3>{note.title}</h3>
+              <h3 style={{textDecoration: completed ? '':'line-through'}}>{note.title}</h3>
              <h3>{note.content}</h3>
               </div>
-             <button className='btn btn-warning m-1'>Edit</button>
+             <button onClick={(title, content, noteId)=> editForm(note.title, note.content, note._id)} className='btn btn-warning m-1'>Edit</button>
              <button className='btn btn-danger m-1'>Delete</button>
+             <button type='checkbox' onClick={()=> toggleTodo(noteId)}>completed</button>
+             {visibility && <div>
+                <h2>Update now</h2>
+                <form >
+          <div className="mb-3">
+            <label htmlFor="title" className="form-label">Title</label>
+            <input type="text" className="form-control" id="title" aria-describedby="emailHelp" onChange={(event)=>setTitle(event.target.value)}/>          
+          </div>
+          <div className="mb-3">
+            <label htmlFor="todo" className="form-label">Todo</label>
+            <input type="text" className="form-control" id="todo" aria-describedby="emailHelp"  onChange={(event)=>setContent(event.target.value)}/>          
+          </div>
+          <button type="submit" onClick={()=> updateNote(noteId)} className="btn btn-info">Edit</button>
+          <button type="submit" className="btn btn-danger">Cancel</button>
+        </form>
+             </div>
+             }
             </li>
           )
         })}
